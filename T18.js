4548df17dfec3617b69f7213308f058054e7b233
SWETHAKIM <154900372+2402Swe@users.noreply.github.com>
2024-02-04 22:45:09 +0530

    Add files via upload

---
+++ T18.js
@@ -0,0 +1,53 @@
+var API = "https://restcountries.com/v3.1/all";
+
+
+var fet = fetch(API)
+  .then((response) => response.json())
+  .then((data) => {
+    
+    data.map((value) => {
+      var spreadOperator = {
+        ...value,
+        name: value.name.common,
+        flag: value.flags.png,
+        code: value.cioc,
+        capital: value.capital,
+        region: value.region,
+        population: value.population,
+        latitude: value.latlng[0],
+        longitude: value.latlng[1]
+
+      };
+      createcountry(spreadOperator);
+    })
+  })
+  
+  
+   
+function createcountry({ name, flag, code, capital, region, population,latitude,longitude }) {
+   
+  document.body.innerHTML += 
+  ` <div class="container">
+    <div class="row">
+     <div class="col-4">
+    <div class="card"  >
+    <h1 id="title" class="text-center">${name}</h1>
+    <img src="${flag}" class="flag" alt="${name}'Flag image">
+ 
+  <div class="card-body car" >
+  <p><span>Population :</span>${population}</p>
+  <p><span>Captial :</span> ${capital}</p>
+  <p><span>Region :</span> ${region}</p>
+  <p><span>Country Code :</span>${code}</p>
+  <a href="#" class="btn btn-primary" onclick=(block(${latitude},${longitude},${name})) >Click for Weather</a>
+ <div id=${name}>   </div>
+  </div>
+  </div>
+ 
+  </div>
+</div>
+</div>
+`
+}
+ 
+document.addEventListener("click",(event) => event.preventDefault())
